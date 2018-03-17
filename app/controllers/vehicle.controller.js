1f69797858d0f63ba328146410f0122b5e7e6e99
charj <dev@charlie.fyi>
2018-03-17 04:12:45 +0000

    Its getting late. Only update to do.

--- app/controllers/vehicle.controller.js
+++ app/controllers/vehicle.controller.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const VehicleModel = require('../models/vehicle.model.js')
 
-exports.create = function(req, res) {
+exports.create = (req, res) => {
     if(!req.body.make) {
         res.status(400).send({error:'Make is required.'})
     }
@@ -21,31 +21,68 @@ exports.create = function(req, res) {
     vehicle.save(function(err, vehicle) {
         if(err) {
             console.log(err);
-            res.status(500).send({message: "An error occurred when creating the Vehicle."});
+            res.status(500).send({error: "An error occurred when creating the Vehicle."});
         } else {
             res.send(vehicle);
         }
     });
 };
 
-exports.read = function(req, res) {
-    // Create and Save a new vehicle
-};
-
-exports.update = function(req, res) {
-    // Updates an existing vehicle
-};
-
-exports.delete = function(req, res) {
-    // Deletes an existing vehicle
-};
-
-exports.list = function(req, res) {
-    VehicleModel.find(function(err, vehicles) {
-        if (err) {
-            res.status(500).send({message: "Some error occurred while retrieving Vehicles."});       
-        } else {
-            res.json(vehicles);
+exports.read = (req, res) => {
+    VehicleModel.findById(req.params.id, (err, vehicle) => {
+        if (!vehicle) {
+            return res.status(404).send({error: "A vehicle with the provided id ("+req.params.id+") could not be found."});
         }
+        if (err) {
+            return res.status(500).send({error: "An error occurred when reading this vehicle."})
+        }
+
+        res.status(200).json(vehicle);
     });
+};
+
+exports.update = (req, res) => {
+    // TODO Build update method
+};
+
+exports.delete = (req, res) => {
+    VehicleModel.findByIdAndRemove({_id: req.params.id}, (err, vehicle) => {
+        if(!vehicle) {
+            return res.status(410).send({error: "This vehicle cannot me found or has already been deleted."})
+        }
+
+        if (err) {
+            return res.status(500).send({error: "An error occurred when deleting this vehicle."})
+        }
+
+        res.status(204).send({success: "Vehicle Deleted Successfully."})
+    })
+}
+
+exports.search = (req, res) => {
+    VehicleModel.find()
+        .select('make model value mot_expiry')
+        .where(req.body.where)
+        .equals(req.body.equals)
+        .sort('created_at')
+        .exec(function(err, vehicles) {
+            if (err) {
+                return res.status(500).send({message: "An error occurred when searching vehicles."})
+            }
+            
+            res.json(vehicles);
+        })
+}
+
+exports.list = (req, res) => {
+    VehicleModel.find()
+        .select('make model value mot_expiry')
+        .sort('manufactured_at')
+        .exec(function(err, vehicles) {
+            if (err) {
+                return res.status(500).send({message: "Some error occurred while retrieving Vehicles."})  
+            }
+            
+            res.json(vehicles);
+        })
 }
