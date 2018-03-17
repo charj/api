31c6b743efd9170d185124dfa2189d275c1d30cb
charj <dev@charlie.fyi>
2018-03-17 04:13:47 +0000

    Adding more routes

--- app/routes/vehicle.routes.js
+++ app/routes/vehicle.routes.js
@@ -2,6 +2,9 @@ module.exports = function(app) {
     var vehicle = require('../controllers/vehicle.controller.js');
 
     // Create a new vehicle
-    app.post('/vehicles', vehicle.create);
-    app.get('/vehicles', vehicle.list);
+    app.post('/vehicle', vehicle.create);
+    app.get('/vehicle', vehicle.list);
+    app.get('/vehicle/:id', vehicle.read);
+    app.post('/vehicle/search', vehicle.search)
+    app.post('/vehicle/delete/:id', vehicle.delete)
 }
